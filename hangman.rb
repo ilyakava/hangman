c198f88d09c5d8882ae8e1c533355fbb27690310
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-06-23 16:45:40 -0400

    Computer guesser mode is working, need to fix how it chooses the letter though

--- hangman.rb
+++ hangman.rb
@@ -15,11 +15,12 @@ class Hangman
 		turns_left = 7
 
 		while curr_board.include?("_") && turns_left > -1
-			puts "You have #{turns_left} turns left. Game status: #{curr_board}"
-			curr_guess = @guesser.make_guess
+			puts "Game status is: #{curr_board}"
+			puts "You have #{turns_left} turns left."
+			curr_guess = @guesser.make_guess(curr_board)
 			reply_to_guess = @master.reply_to_guess(curr_guess)
 
-			if reply_to_guess
+			unless reply_to_guess.empty?
 				curr_board = update_board(curr_board, curr_guess, reply_to_guess)
 			else
 				turns_left -= 1
@@ -31,7 +32,6 @@ class Hangman
 		else
 			puts "Guesser looses. Master, what was the word?"
 			puts "Master says the word was: #{@master.reveal_word}"
-
 		end
 	end
 
@@ -55,7 +55,7 @@ end
 
 class HumanPlayer
 
-	def make_guess
+	def make_guess(board)
 		guess = "invalid_guess"
 		
 		until guess =~ /^.{1}$/
@@ -65,13 +65,25 @@ class HumanPlayer
 		guess
 	end
 
+	def word_length
+		puts "Master, how long is the word you picked?"
+		gets.chomp.to_i
+	end
+
+	def reply_to_guess(curr_guess)
+		puts "The guess is #{curr_guess}"
+		puts "reply with the comma separated indexes that this letter can be found at in your word"
+		gets.chomp.split(",").map { |num| num.to_i }
+	end
 end
 
 class ComputerPlayer
 
 	attr_reader :dict_hash
+	attr_accessor :picked_letters
 
 	def initialize
+		@picked_letters = []
 	end
 
 	def choose_word
@@ -89,11 +101,41 @@ class ComputerPlayer
 		@chosen_word.size.times do |letter_index|
 			matching_positions << letter_index if @chosen_word[letter_index] == letter
 		end
-		matching_positions.empty? ? nil : matching_positions
+		matching_positions
 	end
 
 	def reveal_word
 		@chosen_word
+	end
+
+	def make_regex(curr_board)
+		Regexp.new curr_board.split("").map { |char| char == "_" ? "." : char }.join
+	end
+
+	def prune_dict(curr_board)
+		unless @curr_dict
+			read_dict
+			@curr_dict = @dict_hash
+		end
+		@curr_dict.select! { |index, word| word =~ make_regex(curr_board) }
+	end
+
+	def pick_letter
+		freq = Hash.new { |hash, key| hash[key] = 0 }
+		@curr_dict.values.each do |word|
+			word.chars do |letter|
+				freq[letter] += 1
+			end
+		end
+		freq.reject! { |letter, count| @picked_letters.include?(letter) }
+		freq.sort_by { |letter, count| count }.last[0]
+	end
+
+	def make_guess(curr_board)
+		prune_dict(curr_board)
+		guess = pick_letter
+		@picked_letters << guess
+		guess
 	end
 
 	def read_dict(dictionary = './dictionary.txt', &block)
@@ -108,6 +150,9 @@ class ComputerPlayer
 end
 
 if $0 == __FILE__
-	game = Hangman.new
+	game = Hangman.new(
+		master = HumanPlayer.new,
+		guesser = ComputerPlayer.new
+		)
 	game.play_game
 end
